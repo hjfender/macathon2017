85df8b19bcea55c50b9dbe1041560e023c581296
Fenmaz <tnguyen9@macalester.edu>
2017-02-25 07:26:42 +0000

    Added controller variable

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -69,6 +69,6 @@ class AppointmentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def appointment_params
-      params.require(:appointment).permit(:start_time, :date, :length, :open, :patient_birthdate, :patient_name)
+      params.require(:appointment).permit(:start_time, :date, :length, :open, :patient_birthdate, :patient_name, :doctor_id)
     end
 end
