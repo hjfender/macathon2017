66a30c023e627ce3ad6b9197912e42b9937cc042
Fenmaz <tnguyen9@macalester.edu>
2017-02-25 12:45:20 +0000

    Create distance method for Organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,7 +1,16 @@
+require 'net/http'
+
 class Organization < ActiveRecord::Base
     has_many :doctors
     
     def distance
-        return 10
+        uri = URI('https://maps.googleapis.com/maps/api/distancematrix/json?')
+        params = { :units => 'imperial', :origins => 'Saint Paul,MN', 
+            :destinations => self.location, :key => 'AIzaSyCBlFJqtx98D529iTq02GPysUraNGsPTBM'}
+        uri.query = URI.encode_www_form(params)
+        
+        res = Net::HTTP.get_response(uri)
+        data = JSON.parse res.body
+        return data.rows.elements.distance.value
     end
 end
