ca85aa193168cbe116cba173f80bc005da5bc8a9
Henry Fender <hjfender@gmail.com>
2017-02-25 15:08:06 +0000

    partially done

--- app/controllers/app_pages_controller.rb
+++ app/controllers/app_pages_controller.rb
@@ -1,14 +1,17 @@
 class AppPagesController < ApplicationController
   def signin
-    @doctors = Doctor.all
   end
   
   def filter
     @organizations = Organization.all
     @doctors = Doctor.all
+    @doctor = Doctor.find(1)
     @appointments = Appointment.all
   end
 
   def book
+    @organizations = Organization.all
+    @doctors = Doctor.all
+    @appointments = Appointment.all
   end
 end
